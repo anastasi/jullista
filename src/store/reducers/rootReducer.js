9b12812e54849796f634f0c1415915e403bd55d9
Anastasia Yanvareva <anastasiayanvareva@MAC-ANAYAN.local>
2018-11-06 11:40:13 +0100

    Navbar

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from './authReducer'
 import friendReducer from './friendReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   friend: friendReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 })
 
 export default rootReducer
